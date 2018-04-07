1687718139e9cefd39df3b725105d03a3b87f1b6
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2018-04-07 17:50:58 +0100

    added bibiography break

--- latexcount.py
+++ latexcount.py
@@ -12,6 +12,10 @@ wordCount = 0
 #list of beginnings to ignore in a word count
 toCheck = ["\\begin", "\\end", "\\documentclass", "\\usepackage", "\\title", "\\date", "\\chapter", "\\section", "\\subsection", "\\cite", "\\newcommand", "\\maketitle", "\\geometry"]
 
+toBreak = ["\\chapter{Bibliography}"]
+
+to_break = False
+
 #read .tex file and update wordCount
 with open(filename, "r") as texFile:
     for line in texFile:
@@ -20,12 +24,17 @@ with open(filename, "r") as texFile:
         for word in words[0]:
             is_in = False
             counter = 0
-            while is_in == False and counter < len(toCheck):
-                if word[0:len(toCheck[counter])] == toCheck[counter]:
-                    is_in = True
-                counter += 1
-            if is_in == False:
-                wordCount += 1
+            
+            if word[0:len(toBreak[0])] == toBreak[0]:
+                to_break = True
+            
+            if to_break == False:    
+                while is_in == False and counter < len(toCheck):
+                    if word[0:len(toCheck[counter])] == toCheck[counter]:
+                        is_in = True
+                    counter += 1
+                if is_in == False:
+                    wordCount += 1
                 
 #output wordcount.txt file
 with open("wordcount.txt", "w") as outputFile:
